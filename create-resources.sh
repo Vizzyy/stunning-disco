6b6caa3e852b08c53fdb02cc6027ce7d9e4d7a5f
Vizzyy <barnabas.vizy@gmail.com>
2021-04-13 19:23:27 -0400

    Almost finished initial route + fully working api and routing

--- create-resources.sh
+++ create-resources.sh
@@ -2,17 +2,23 @@
 
 FUNC_NAME="stunning-disco"
 
-echo "Deleting existing stack: $FUNC_NAME"
-aws cloudformation delete-stack --stack-name $FUNC_NAME # delete stack
+while [ -n "$1" ]
+do
+  case "$1" in
+    -d) echo "Deleting existing stack: $FUNC_NAME"; \
+      aws cloudformation delete-stack --stack-name $FUNC_NAME; \
+      echo "Waiting for stack deletion to finish..."; \
+      aws cloudformation wait stack-delete-complete --stack-name $FUNC_NAME ;;
+    *) echo "$1 is not an option" ;;
+  esac
+  shift
+done
 
-#zip -r lambda_function.zip availability-canary.py mysql* google proto* six* -q
 echo "Packaging template..."
 sam package --s3-bucket vizzyy-packaging --output-template-file packaged.yml
-echo "Waiting for stack deletion to finish..."
-aws cloudformation wait stack-delete-complete --stack-name $FUNC_NAME
+
 echo "Deploying new stack..."
 sam deploy --template-file packaged.yml --stack-name $FUNC_NAME --capabilities CAPABILITY_IAM && echo "Finished deploying!"
 
-#rm lambda_function.zip
 rm packaged.yml
 echo "Finished cleanup."
