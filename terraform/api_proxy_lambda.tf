ce0cd99f5e6c5e01a49433025ce8e9feb10d7162
Vizzyy <barnabas.vizy@gmail.com>
2021-06-27 15:28:13 -0400

    Working first lambda

--- terraform/api_proxy_lambda.tf
+++ terraform/api_proxy_lambda.tf
@@ -1,70 +1,34 @@
-resource "aws_lambda_function" "example" {
+resource "aws_lambda_function" "api_proxy_lambda" {
   function_name = "api_proxy_lambda"
   filename = "../http-request.zip"
   handler = "http-request.lambda_handler"
   runtime = "python3.8"
-  role = aws_iam_role.iam_for_lambda.arn
-
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_logs,
-    aws_cloudwatch_log_group.example,
-  ]
+  role = aws_iam_role.api_proxy_lambda_role.arn
 }
 
-//resource "aws_iam_role" "lambda_exec" {
-//  name = "api_proxy_lambda_exec_role"
-//  assume_role_policy = file("data/api_proxy_lambda_exec_role.json")
-//}
-
-resource "aws_cloudwatch_log_group" "example" {
+resource "aws_cloudwatch_log_group" "api_proxy_lambda_logs" {
   name              = "/aws/lambda/api_proxy_lambda"
-  retention_in_days = 14
+  retention_in_days = 7
 }
 
-# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    },
-    {
-      "Action": [
-        "ssm:GetParametersByPath"
-      ],
-      "Effect": "Allow",
-      "Resource": "${var.ssm_resources}"
-    },
-    {
-      "Action": [
-        "sqs:SendMessage"
-      ],
-      "Effect": "Allow",
-      "Resource": "${var.sqs_resources}"
-    }
+resource "aws_iam_role" "api_proxy_lambda_role" {
+  name = "api_proxy_lambda_role"
+  assume_role_policy = file("data/lambda_trust_relationship.json")
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    aws_iam_policy.api_proxy_lambda_exec_policy.arn
   ]
 }
-EOF
+
+data "template_file" "api_proxy_lambda_exec_policy_template" {
+  template = file("data/lambda_api_proxy_exec_role.json.tpl")
+  vars = {
+    ssm_resources = var.ssm_resources,
+    sqs_resources = var.sqs_resources
+  }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-  assume_role_policy = file("data/api_proxy_lambda_exec_role.json")
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
+resource "aws_iam_policy" "api_proxy_lambda_exec_policy" {
+  name = "api_proxy_lambda_exec_policy"
+  policy = data.template_file.api_proxy_lambda_exec_policy_template.rendered
 }
